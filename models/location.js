9c4cdc8dbb88003e3448023a64cfa717f34b6fea
m.alsheri <m.alsheri>
2016-08-23 10:46:35 +0300

    fixed uniqueness bug in location

--- models/location.js
+++ models/location.js
@@ -4,13 +4,11 @@ const mongoose = require("mongoose");
 const locationSchema = new mongoose.Schema({
     country: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     city: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     }
 });
 
