7906dc9b84f1b57c1f6b5b6cb6d31bca675a5787
Alex Dorner <alex_1115@outlook.com>
2016-11-02 18:39:36 +0100

    when register admin, modify route to signup.

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,6 @@ router.use("/upload", require("./upload"));
 router.use("/", require("./util"));
 router.use("/tags?", require("./tag"));
 router.use("/systemVariables?", require("./systemVariable"));
-router.use("/admin", require("./admin"));
+router.use("/signup", require("./signup"));
 
 module.exports = router;
