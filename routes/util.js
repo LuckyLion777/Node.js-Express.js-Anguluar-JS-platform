db29f6e9b48fdec5860168274c741f0439613290
Evgeny Bartashevich <earx@yandex.ru>
2016-12-09 13:27:25 +0300

    /api/check/email/:email re-write the function to check the email if exist in our database or not. /api/check/username/:username re-write the function to check the email if exist in our database or not.

--- routes/util.js
+++ routes/util.js
@@ -5,6 +5,12 @@ const auth = require("../util/auth/index");
 const jwtGenerator = require("../util/jwtGenerator");
 
 
+const STATUS_SUCCESS =  true;
+const STATUS_FAILED  =  false;
+
+const USER_EXISTS       =  'user exists';
+const USER_DOESNTEXIST  =  'user does not exist';
+
 router.get("/users", passport.authenticate("jwt", { session: false }), auth.can("List Users"), (req, res, next) => {
     res.locals.promise = models.User.getUsers();
     return next();
@@ -16,13 +22,54 @@ router.get("/admins", passport.authenticate("jwt", { session: false }), auth.can
 });
 
 router.get("/check/email/:email", (req, res, next) => {
-    res.locals.promise = models.User.checkEmail(req.params.email);
-    return next();
+    
+    models.User.checkEmail(req.params.email)
+        .then(result =>  {
+
+            if (result) {
+                
+                return res.send({status: STATUS_SUCCESS, message: USER_EXISTS });
+                
+            }
+            else{
+                
+                return res.send({status: STATUS_FAILED, message: USER_DOESNTEXIST });
+            }
+
+        }, err => {
+            
+            throw(err);
+        })
+        .catch(err => {
+            //TODO: log
+            throw(err);
+        });
 });
 
 router.get("/check/username/:username", (req, res, next) => {
-    res.locals.promise = models.User.checkUsername(req.params.username);
-    return next();
+    
+    models.User.checkUsername(req.params.username)
+        .then(result =>  {
+
+            if (result) {
+                
+                return res.send({status: STATUS_SUCCESS, message: USER_EXISTS });
+                
+            }
+            else{
+                
+                return res.send({status: STATUS_FAILED, message: USER_DOESNTEXIST });
+            }
+
+        }, err => {
+            
+            throw(err);
+        })
+        .catch(err => {
+            //TODO: log
+            throw(err);
+        });
+        
 });
 
 module.exports = router;
