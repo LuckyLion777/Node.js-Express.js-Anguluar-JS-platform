24da0643d86072f7b367e1dae75a19ea839b08fd
Evgeny Bartashevich <earx@yandex.ru>
2016-12-13 18:04:31 +0300

    .Make it secure ,but still public function .all types of popular image formats EXCEPT SVGs .the user auth token must be sent along with the the upload request

--- config/multer.js
+++ config/multer.js
@@ -1,11 +1,65 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const fs = require('fs');
+
+const uploadpath = path.join(__dirname, "../uploads");
 
 const diskStorage = multer.diskStorage({
-    destination: path.join(__dirname, "../uploads"),
+    destination: uploadpath,
     filename: (req, file, callback) => callback(null, crypto.pseudoRandomBytes(10).toString("hex") + file.originalname)
 });
 
 
-module.exports = multer({ storage: diskStorage });
+//check if upload folder is writable
+fs.access(uploadpath, fs.W_OK, function(err) {
+
+  if(err){
+    console.error("upload folder is not writeable!", err);
+    process.exit(1);
+  }
+});
+
+//@link https://github.com/expressjs/multer
+module.exports = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 1024*1024*2 //2 Mb
+        //fieldNameSize	Max field name size	100 bytes
+        //fieldSize	Max field value size	1MB
+        //fields	Max number of non-file fields	Infinity
+        //fileSize	For multipart forms, the max file size (in bytes)	Infinity
+        //files	For multipart forms, the max number of file fields	Infinity
+        //parts	For multipart forms, the max number of parts (fields + files)	Infinity
+        //headerPairs	For multipart forms, the max number of header key=>value pairs to parse	2000
+    },
+    fileFilter: (req, file, cb) => {
+        
+        switch (file.mimetype) {
+            
+            case 'image/jpeg':
+            case 'image/pjpeg':
+            case 'image/x-portable-bitmap':
+            case 'image/x-pict':
+            case 'image/pict':
+            case 'image/x-pcx':
+            case 'image/tiff':
+            case 'image/x-tiff':
+            case 'image/x-icon':
+            case 'image/xpm':
+            case 'image/png':
+            case 'image/bmp':
+            case 'image/gif':
+            case 'image/x-windows-bmp':
+
+                cb(null, true); //accept image
+                break;
+            
+            default:
+                cb(new Error('Restricted file type'));
+
+        }
+
+    }
+
+});
--- routes/upload.js
+++ routes/upload.js
@@ -5,11 +5,14 @@ const passport = require("passport");
 const upload = require("../config/multer");
 
 
-router.post("/", upload.single("photo"), passport.authenticate("jwt", { session: false }), auth.can("Upload Photo"), (req, res, next) => {
+router.post("/", upload.single("photo"),
+    passport.authenticate("jwt", { session: false }),
+    auth.can("Upload Photo"),
+    (req, res, next) => {
 
-    if(req.file) req.body.photo = { filename: req.file.filename };
-    res.locals.promise = models.Upload.upload(req.body);
-    return next();
+        if(req.file) req.body.photo = { filename: req.file.filename };
+        res.locals.promise = models.Upload.upload(req.body);
+        return next();
 });
 
 
