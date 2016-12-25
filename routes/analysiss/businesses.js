3a91607b95ca4512a5a2e6388345d60d995b7909
Mona <Mona>
2016-12-25 17:27:15 +0300

    get businesses by date

--- routes/analysiss/businesses.js
+++ routes/analysiss/businesses.js
@@ -1,5 +1,6 @@
 const models        = require("../../models");
 const router        = require("express").Router();
+const moment        = require("moment");
 
 // GET: Number of all Business
 router.get("/", (req, res, next) => {
@@ -25,6 +26,61 @@ router.get("/published", (req, res, next) => {
 // GET: Number of pending businesses
 router.get("/pending", (req, res, next) => {
     models.Business.find().count({status:"PENDING"},function(err, count){
+	    res.send({count: count});
+	});
+});
+
+//  GET: Number of of businesses (today, this week, this month, this year) 
+// $gte: moment().startOf('day').toDate()
+
+router.get("/date/today", (req, res, next) => {
+    var promise = models.Business.find(
+        {createdAt: {
+                $gte: moment().startOf('day').toDate()
+            }
+        }            
+    ).count(function(err, count){
+	    res.send({count: count});
+	});
+    
+    //res.locals.promise = promise;
+    
+    //return next();
+});
+
+router.get("/date/week", (req, res, next) => {
+    models.Business.find(
+        {
+            createdAt: {
+                $gte: moment().weekday(-7).toDate(),
+                $lt: moment().startOf('day').toDate()
+            }
+        }            
+        ).count(function(err, count){
+	       res.send({count: count});
+	    });
+});
+
+router.get("/date/month", (req, res, next) => {
+    models.Business.find(
+        {createdAt: {
+                $gte: moment().weekday(-31).toDate(),
+                $lt: moment().startOf('day').toDate()
+            }
+        }            
+        ).count(function(err, count){
+	    res.send({count: count});
+	});
+});
+
+router.get("/date/year", (req, res, next) => {
+    models.Business.find(
+        {createdAt: {
+                $gte: moment().weekday(-365).toDate(),
+                $lt: moment().startOf('day').toDate()
+            }
+        }            
+        ).count(function(err, count){
 	    res.send({count: count});
 	});
 });
