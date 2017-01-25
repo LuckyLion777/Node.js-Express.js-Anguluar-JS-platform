a07ac70af4ac3a3cc4cb7acc46a087c7c5c24056
Mona <Mona>
2017-01-25 16:03:23 +0300

    user count

--- routes/analysiss/users.js
+++ routes/analysiss/users.js
@@ -10,21 +10,21 @@ router.get("/", (req, res, next) => {
 
 // GET: Number of active users
 router.get("/active", (req, res, next) => {
-    models.User.find().count({status:"ACTIVE"},function(err, count){
+    models.User.count({status:"ACTIVE"},function(err, count){
 	    res.send({count: count});
 	});
 });
 
 // GET: Number of buisiness users
 router.get("/buisiness", (req, res, next) => {
-    models.User.find().count({userType:"Business User"},function(err, count){
+    models.User.count({userType:"Business User"},function(err, count){
 	    res.send({count: count});
 	});
 });
 
 // GET: Number of regular users
 router.get("/regular", (req, res, next) => {
-    models.User.find().count({userType:"User"},function(err, count){
+    models.User.count({userType:"User"},function(err, count){
 	    res.send({count: count});
 	});
 });
